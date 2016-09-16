299596537eab84afa8e741580f7c8f1cc5ed322c
Hemanth.HM <hemanth.hm@gmail.com>
2016-09-16 12:38:43 +0530

    Listing the element names

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import pt from 'periodic-table';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>elementeum</h2>
+    return <div>
+      {Object.keys(pt.elements).map(element => {
+        return <div>
+          {element}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      })}
+    </div>
   }
 }
 
