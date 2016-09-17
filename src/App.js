eb1268aa184abfd7538cb0637ff645c31068f2ef
hemanth <hemanth.hm@gmail.com>
2016-09-17 17:18:40 +0000

    Basic card styling for elements.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import pt from 'periodic-table';
 
 class App extends Component {
   render() {
-    return <div>
+    return <div className='main'>
       {Object.keys(pt.elements).map(element => {
-        return <div>
+        return <div className='element' id={element}>
           {element}
         </div>
       })}
