50418bc121c66f71a3c89193a6035e35fab4c18d
hemanth <hemanth.hm@gmail.com>
2016-09-18 09:19:26 +0000

    Elementinfo component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ElementInfo from './ElementInfo.jsx'
 import './App.css';
 import pt from 'periodic-table';
 
@@ -6,9 +7,12 @@ class App extends Component {
   render() {
     return <div className='main'>
       {Object.keys(pt.elements).map(element => {
-        return <div className='element' id={element}>
-          {element}
-        </div>
+        return (
+           <div>
+            <div className='element' id={element}> {element} </div>
+            <ElementInfo info={...pt.elements[element]}/>
+           </div>
+        );
       })}
     </div>
   }
