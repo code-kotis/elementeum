98b875731b9a38aacaa8f3866ed09436db89c07c
hemanth <hemanth.hm@gmail.com>
2016-09-20 15:59:50 +0000

    brace

--- src/ElementInfo.jsx
+++ src/ElementInfo.jsx
@@ -4,9 +4,9 @@ import './ElementInfo.css';
 class ElementInfo extends Component {
   render() {
     return <div className='element--info'>
-       Object.keys(this.props.info, elemProp => {
+        {Object.keys(this.props.info, elemProp => {
            return <span className={info} id={info}>{this.props.info[elemProp] || null}</span>
-        })
+        })}
     </div>
   }
 }
